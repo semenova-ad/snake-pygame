5806e00eff5e7b576ecbfd6a4643156e5a26d42c
Alyssa Semenova <semenova.ad@misis.ru>
2025-06-10 14:11:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ while not game_over:
         if event.type == pg.QUIT:
             game_over = True
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
+            if event.key == pg.K_LEFT and direction != "right":
                 direction = "left"
-            if event.key == pg.K_RIGHT:
+            if event.key == pg.K_RIGHT and direction != "left":
                 direction = "right"
-            if event.key == pg.K_UP:
+            if event.key == pg.K_UP and direction != "down":
                 direction = "up"
-            if event.key == pg.K_DOWN:
+            if event.key == pg.K_DOWN and direction != "up":
                 direction = "down"
 
     # Движение змейки
@@ -60,10 +60,27 @@ while not game_over:
             apple_x = randint(1, 19) * 40
             apple_y = randint(1, 11) * 40
 
+    if x < 0 or x >= 800 or y < 0 or y >= 480:
+        game_over = True
+        message = font.render("Ты проиграл", True, WHITE)
+        disp.blit(message, [300, 20])
+        pg.display.update()
+        pg.time.delay(2000)
+        break
+
+    if len(snake) > 4 and snake[-1] in snake[:-1]:
+        game_over = True
+        message = font.render("Ты проиграл", True, WHITE)
+        disp.blit(message, [300, 20])
+        pg.display.update()
+        pg.time.delay(2000)
+        break
+
     # Рисуем текущий кадр
     disp.fill(BLACK)
     for i in range(len(snake)):
         pg.draw.rect(disp, GREEN, [snake[i][0], snake[i][1], 40, 40])
+
     pg.draw.rect(disp, RED, [apple_x, apple_y, 40, 40])
 
     message = font.render("Счёт: " + str(score), True, WHITE)
